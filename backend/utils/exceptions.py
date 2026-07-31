ec5b0b22a7b1b1bfef81d799c70aeb98a5c2d06e
Caleb-Asia <calebasia1811@gmail.com>
2026-07-31 11:00:11 +0200

    Deleted back end stuff

--- backend/utils/exceptions.py
+++
@@ -1,7 +0,0 @@
-class ScraperError(Exception):
-    def __init__(self, market, url, reason, status_code=None):
-        self.market = market
-        self.url = url
-        self.reason = reason
-        self.status_code = status_code
-        super().__init__(f"[{market}] {reason} (url: {url})")
